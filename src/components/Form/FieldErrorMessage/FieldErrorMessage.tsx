bdef7b2a529d20cf20cb54de694d92fd4357f54e
Alan <mortoni.alan@hotmail.com>
2021-12-05 19:49:48 +1000

    feat(field error message): field error message component

---
+++ src/components/Form/FieldErrorMessage/FieldErrorMessage.tsx
@@ -0,0 +1,18 @@
+import { ErrorMessage } from '@hookform/error-message'
+import { FieldErrors } from 'react-hook-form'
+import { Hint } from 'components/Typography'
+import tw from 'twin.macro'
+
+const FieldErrorMessage = ({ errors, name, ...props }: { errors: FieldErrors; name: string }) => (
+  <ErrorMessage
+    name={name}
+    errors={errors}
+    render={({ message }) => (
+      <Hint css={tw`text-error-600 text-opacity-75`} {...props}>
+        {message}
+      </Hint>
+    )}
+  />
+)
+
+export default FieldErrorMessage
