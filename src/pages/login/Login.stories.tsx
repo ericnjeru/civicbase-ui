2341c8298d808202d4983d45018a6b0339d8726a
Alan <mortoni.alan@hotmail.com>
2021-12-05 19:51:19 +1000

    fix(login page): new style

---
+++ src/pages/login/Login.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta } from '@storybook/react'
+import Login from './Login'
+
+export default {
+  title: 'Forms/Login',
+  component: Login,
+} as Meta
+
+export const Basic = () => <Login />
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,122 +1,68 @@
-import { useState, FC } from 'react'
-import tw from 'twin.macro'
+import { FC, useState } from 'react'
+import { BsArrowLeft } from 'react-icons/bs'
 import { RouteComponentProps } from '@reach/router'
-import { OutlinedButton } from 'components/Button'
 import Card from 'components/Card'
-import Typography, { Title, Caption } from 'components/Typography'
-import Label from 'components/Form/Label'
-import Input from 'components/Form/Input'
-
-// This idea is coming from: https://dribbble.com/shots/5311359-Diprella-Login
-
-const SignupForm = () => {
-  return (
-    <div css={tw`p-5`}>
-      <Title css={tw`text-center`}>Create Account</Title>
-
-      <div css={tw`grid grid-cols-1 gap-4 align-middle`}>
-        <div>
-          <Label>Name</Label>
-          <Input />
-        </div>
-
-        <div>
-          <Label>Email</Label>
-          <Input />
-        </div>
-
-        <div>
-          <Label>Password</Label>
-          <Input />
-        </div>
-
-        <OutlinedButton onClick={() => {}}>Sign Up</OutlinedButton>
-      </div>
-    </div>
-  )
-}
-
-const LoginForm = () => {
-  return (
-    <div css={tw`p-5`}>
-      <Title css={tw`text-center`}>Sign in to QVSR</Title>
-
-      <div css={tw`grid grid-cols-1 gap-4 align-middle`}>
-        <div>
-          <Label>Email</Label>
-          <Input />
-        </div>
-
-        <div>
-          <Label>Password</Label>
-          <Input />
-        </div>
-
-        <Caption css={tw`text-center`}>Forgot your password?</Caption>
-
-        <OutlinedButton onClick={() => {}}>Sign In</OutlinedButton>
-      </div>
-    </div>
-  )
-}
-
-const LoginBanner = ({ handleToggle }: { handleToggle: () => void }) => {
-  return (
-    <div css={tw`p-5 flex justify-between flex-col h-full`}>
-      <div>
-        <Title>Hello, Friend!</Title>
-        <Typography css={tw`text-center max-w-xs`}>Enter your personal details and start a journey with us.</Typography>
-      </div>
-      <OutlinedButton onClick={handleToggle}>Login</OutlinedButton>
-    </div>
-  )
-}
-const SignupBanner = ({ handleToggle }: { handleToggle: () => void }) => {
-  return (
-    <div css={tw`p-5 flex justify-between flex-col h-full`}>
-      <div>
-        <Title>Welcome Back!</Title>
-        <Typography css={tw`text-center max-w-xs`}>
-          To keep connected with us please login with your personal info.{' '}
-        </Typography>
-      </div>
-      <OutlinedButton onClick={handleToggle}>Sign up</OutlinedButton>
-    </div>
-  )
-}
+import tw, { theme } from 'twin.macro'
+import { Title } from 'components/Typography'
+import { IconButton } from 'components/Button'
+import LoginForm from 'components/forms/Login'
+import SignupForm from 'components/forms/Signup'
 
 const Login: FC<RouteComponentProps> = () => {
-  const [clicked, setClicked] = useState(false)
+  const [toggle, setToggle] = useState(true)
 
   return (
-    <div css={tw`flex justify-center`}>
-      <Card css={tw`max-w-screen-md w-full h-96 relative`}>
-        <div
-          style={{ top: '-1px', right: '-1px', width: clicked ? '24.5rem' : '28rem' }}
-          css={[
-            tw`h-96 w-96 absolute border-0`,
-            clicked && tw`rounded-l-md transition-all ease-in-out duration-700 transform -translate-x-96 `,
-            !clicked && tw` rounded-r-md transition-all ease-in-out duration-700`,
-          ]}
-        >
-          {clicked ? <SignupForm /> : <LoginForm />}
+    <div css={tw`flex justify-center h-full mobile:items-start items-center`}>
+      <Card
+        css={[
+          tw`rounded-3xl p-0 relative border-0 overflow-hidden w-96`,
+          tw`bg-gradient-to-tr from-blue-300 via-blue-500 to-pink-400`,
+        ]}
+        style={{ height: 800 }}
+      >
+        <div css={tw`text-center mt-8`}>
+          <div css={[tw`opacity-0`, toggle && tw`transition-all ease-in-out duration-700 opacity-100`]}>
+            {toggle && <Title css={tw`text-white`}>Civicbase</Title>}
+          </div>
+
+          <div
+            css={[
+              tw`flex justify-between items-center opacity-0 mx-2`,
+              !toggle && tw`transition-all ease-in-out duration-700 opacity-100`,
+            ]}
+          >
+            <IconButton onClick={() => setToggle(true)}>
+              <BsArrowLeft size={28} color={theme`colors.white`} />
+            </IconButton>
+            <div css={tw`w-full`}>
+              <Title css={tw`text-white m-0`}>Sign Up</Title>
+            </div>
+          </div>
         </div>
 
-        <div
-          style={{ top: '-1px', left: '-1px' }}
+        <Card
           css={[
-            tw`h-96 w-96 absolute border-0 z-10`,
-            tw`bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500`,
-            clicked && tw`rounded-r-md transition-all ease-in-out duration-700 transform translate-x-96 `,
-            !clicked && tw` rounded-l-md w-80 transition-all ease-in-out duration-700`,
+            tw`absolute top-60 w-full h-auto bottom-0 rounded-none border-0`,
+            tw`flex flex-col pt-12`,
+            !toggle && tw`transition-all ease-in-out duration-700 transform translate-y-full opacity-0`,
+            toggle && tw`transition-all ease-in-out duration-700 transform -translate-y-0 opacity-100`,
           ]}
+          style={{ borderTopLeftRadius: toggle ? 56 : 0 }}
         >
-          {clicked ? (
-            <LoginBanner handleToggle={() => setClicked(!clicked)} />
-          ) : (
-            <SignupBanner handleToggle={() => setClicked(!clicked)} />
-          )}
-        </div>
+          <LoginForm handleToggle={() => setToggle(false)} toggle={toggle} />
+        </Card>
+
+        <Card
+          css={[
+            tw`absolute top-32 w-full h-auto bottom-0 rounded-tr-none rounded-none border-0`,
+            tw`flex flex-col transform translate-y-full pt-12`,
+            !toggle && tw`transition-all ease-in-out duration-700 transform -translate-y-0 opacity-100`,
+            toggle && tw`transition-all ease-in-out duration-700 transform translate-y-full opacity-0`,
+          ]}
+          style={{ borderTopLeftRadius: !toggle ? 56 : 0 }}
+        >
+          <SignupForm toggle={toggle} />
+        </Card>
       </Card>
     </div>
   )
