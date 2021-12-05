a693a902126da00cb4e2b26715721dbee5d18af3
Alan <mortoni.alan@hotmail.com>
2021-12-05 19:48:52 +1000

    fix(label): label input to handle darkmode

--- src/components/Form/Label/Label.tsx
+++ src/components/Form/Label/Label.tsx
@@ -1,5 +1,5 @@
 import tw, { styled } from 'twin.macro'
 
-const Label = styled.label(() => [tw`block font-semibold text-gray-900 text-sm my-1.5`])
+const Label = styled.label(() => [tw`block font-semibold text-gray-900 text-sm my-1.5 dark:text-white`])
 
 export default Label
