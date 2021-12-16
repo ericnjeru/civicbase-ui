5d617637572845487071c3dd6b90ebc1f367a0c3
Alan <mortoni.alan@hotmail.com>
2021-12-16 18:55:21 +1000

    feat(dashboard): add survey navigates user to create survey page

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -3,10 +3,13 @@ import { FC } from 'react'
 import { RouteComponentProps } from '@reach/router'
 import SurveyCard from 'components/SurveyCard'
 import AddButton from 'components/AddButton'
+import { useNavigate } from '@reach/router'
 
 const Dashboard: FC<RouteComponentProps> = () => {
+  const navigate = useNavigate()
+
   return (
-    <div css={tw`grid mobile:grid-cols-1 tablet:grid-cols-2 grid-cols-3  gap-8`}>
+    <div css={tw`grid mobile:grid-cols-1 tablet:grid-cols-2 grid-cols-3  gap-8 py-8 mx-4`}>
       <SurveyCard status="pilot" title="Pilot Survey" method="quadratic" />
       <SurveyCard status="published" title="Published Survey" method="linear" />
       <SurveyCard status="finished" title="Finished Survey" method="quadratic" />
@@ -15,7 +18,7 @@ const Dashboard: FC<RouteComponentProps> = () => {
       <SurveyCard status="finished" title="Finished Survey" method="quadratic" />
       <SurveyCard status="pilot" title="Pilot Survey" method="quadratic" />
       <SurveyCard status="published" title="Published Survey" method="linear" />
-      <AddButton onClick={() => console.log('clicked')}>+ Create Survey</AddButton>
+      <AddButton onClick={() => navigate(`/create-survey`)}>+ Create Survey</AddButton>
     </div>
   )
 }
