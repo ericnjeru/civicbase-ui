bf365730af60ceae5de6af7f59f88d7d8883732e
Alan <mortoni.alan@hotmail.com>
2022-01-13 20:21:05 +1000

    feat(dashboard): get surveys and show them

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,23 +1,18 @@
 import tw from 'twin.macro'
 import { FC } from 'react'
-import { RouteComponentProps } from '@reach/router'
+import { RouteComponentProps, useNavigate } from '@reach/router'
 import SurveyCard from 'components/SurveyCard'
 import AddButton from 'components/AddButton'
-import { useNavigate } from '@reach/router'
+import { useSurveys, SurveyState } from 'contexts/surveys'
 
 const Dashboard: FC<RouteComponentProps> = () => {
   const navigate = useNavigate()
+  const { surveys } = useSurveys()
 
   return (
     <div css={tw`grid mobile:grid-cols-1 tablet:grid-cols-2 grid-cols-3  gap-8 py-8 mx-4`}>
-      <SurveyCard status="pilot" title="Pilot Survey" method="quadratic" />
-      <SurveyCard status="published" title="Published Survey" method="linear" />
-      <SurveyCard status="finished" title="Finished Survey" method="quadratic" />
-      <SurveyCard status="pilot" title="Pilot Survey" method="quadratic" />
-      <SurveyCard status="published" title="Published Survey" method="linear" />
-      <SurveyCard status="finished" title="Finished Survey" method="quadratic" />
-      <SurveyCard status="pilot" title="Pilot Survey" method="quadratic" />
-      <SurveyCard status="published" title="Published Survey" method="linear" />
+      {surveys && surveys.map((survey: SurveyState) => <SurveyCard survey={survey} key={survey.id} />)}
+
       <AddButton onClick={() => navigate(`/create-survey`)}>+ Create Survey</AddButton>
     </div>
   )
