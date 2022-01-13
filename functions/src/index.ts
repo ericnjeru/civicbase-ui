ac48179c9070da45544cf71c54582e80e1c9e332
Alan <mortoni.alan@hotmail.com>
2022-01-13 20:43:39 +1000

    feat(functions index): add endpoints for survey and answer

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,18 @@ import * as express from 'express'
 import * as cors from 'cors'
 import { auth } from './utils/auth'
 import { signup, reset, login, details, logout } from './api/user'
-import { createSurvey, updateSurvey } from './api/survey'
+import {
+  createSurvey,
+  updateSurvey,
+  surveys,
+  publishSurvey,
+  finishSurvey,
+  cloneSurvey,
+  deleteSurvey,
+  getSurvey,
+} from './api/survey'
+
+import { createAnswer } from './api/answer'
 
 const app = express()
 
@@ -15,7 +26,15 @@ app.post('/reset', reset)
 app.get('/user', auth, details)
 app.get('/logout', auth, logout)
 
+app.get('/survey/:surveyId', getSurvey)
+app.get('/surveys', auth, surveys)
 app.post('/createSurvey', auth, createSurvey)
-app.post('/updateSurvey', auth, updateSurvey)
+app.post('/updateSurvey/:surveyId', auth, updateSurvey)
+app.get('/publish/:surveyId', auth, publishSurvey)
+app.get('/finish/:surveyId', auth, finishSurvey)
+app.get('/clone/:surveyId', auth, cloneSurvey)
+app.delete('/delete/:surveyId', auth, deleteSurvey)
+
+app.post('/createAnswer', createAnswer)
 
 exports.app = functions.https.onRequest(app)
