ea8c4b50a5d84c9b9012a3ca3e502563a762993d
Alan <mortoni.alan@hotmail.com>
2022-01-13 20:16:09 +1000

    fix(storage): hasToken is a function

--- src/utilities/storage.ts
+++ src/utilities/storage.ts
@@ -3,6 +3,6 @@ const KEY = '__civicbase__'
 export default {
   getToken: () => window.localStorage.getItem(KEY),
   setToken: (token: string) => window.localStorage.setItem(KEY, token),
-  hasToken: !!window.localStorage.getItem(KEY),
+  hasToken: () => !!window.localStorage.getItem(KEY),
   clearToken: () => window.localStorage.removeItem(KEY),
 }
