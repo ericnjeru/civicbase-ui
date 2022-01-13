0d80cff606332e244516c1a164831b74d87ff858
Alan <mortoni.alan@hotmail.com>
2022-01-13 20:18:12 +1000

    fix(auth): improve some logic

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,27 +1,25 @@
-import * as api from './api'
+import client from './api'
 import storage from 'utilities/storage'
 import { LoginRequest, SignupRequest, ResetRequest } from '../../types/request.d'
 import { LoginReponse, SignupResponse } from '../../types/response.d'
 
-export const signup = ({ name, email, password }: SignupRequest): Promise<SignupResponse> => {
-  return api.client('signup', { body: { name, email, password } }).then((data) => data)
-}
+export const signup = ({ name, email, password }: SignupRequest): Promise<SignupResponse> =>
+  client('signup', { body: { name, email, password } }).then((data) => data)
 
 export const getUser = () => {
   if (!storage.hasToken) {
     return Promise.resolve(null)
   }
 
-  return api.client('user').then(({ user }) => user)
+  return client('user').then(({ user }) => user)
 }
 
 export const login = ({ email, password }: LoginRequest): Promise<LoginReponse> =>
-  api.client('login', { body: { email, password } }).then(({ user, token }) => {
+  client('login', { body: { email, password } }).then(({ user, token }) => {
     storage.setToken(token)
     return user
   })
 
-export const logout = () => api.client('logout').then(() => storage.clearToken())
+export const logout = () => client('logout').then(() => storage.clearToken())
 
-export const reset = ({ email }: ResetRequest) =>
-  api.client('reset', { body: { email } }).then(({ message }) => message)
+export const reset = ({ email }: ResetRequest) => client('reset', { body: { email } }).then(({ message }) => message)
