3e304094b5eadcc928d322c78c605456cb431664
Alan <mortoni.alan@hotmail.com>
2022-01-13 20:26:53 +1000

    feat(banner context): add banner context

---
+++ src/contexts/banner.tsx
@@ -0,0 +1,68 @@
+import { createContext, ReactElement, useCallback, useContext, useState } from 'react'
+
+interface Banner {
+  show?: boolean
+  icon?: any
+  title?: string
+  subtitle?: string
+  actionText?: string
+  action?: () => void
+  duration?: number
+}
+
+interface BannerContextProps extends Banner {
+  trigger: (banner?: Banner) => void
+  dismiss: () => void
+}
+
+const initialData: BannerContextProps = {
+  show: false,
+  icon: undefined,
+  title: undefined,
+  subtitle: undefined,
+  actionText: undefined,
+  action: undefined,
+  duration: 5000,
+  trigger: () => {},
+  dismiss: () => {},
+}
+
+const BannerContext = createContext(initialData)
+
+export const BannerProvider = (props: any): ReactElement => {
+  const [banner, setBanner] = useState<Banner>({
+    show: false,
+    icon: undefined,
+    title: undefined,
+    subtitle: undefined,
+    actionText: undefined,
+    action: undefined,
+    duration: 5000,
+  })
+
+  const trigger = useCallback((newBanner?: Banner) => {
+    const duration = newBanner?.duration || 5000
+
+    setBanner({ ...newBanner, show: true, duration })
+
+    setTimeout(() => {
+      setBanner({ show: false, duration })
+    }, duration)
+  }, [])
+
+  const dismiss = useCallback(() => {
+    setBanner({ ...banner, show: false })
+  }, [banner])
+
+  return <BannerContext.Provider value={{ trigger, dismiss, ...banner }} {...props} />
+}
+
+export const useBanner = () => {
+  const context = useContext(BannerContext)
+
+  if (context === undefined) {
+    throw new Error('useBanner() must be use within a BannerProvider')
+  }
+
+  return context
+}
