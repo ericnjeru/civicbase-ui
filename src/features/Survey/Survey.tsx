f75ed9d57e6fcf0d87105415810df102fb86d0f5
Alan <mortoni.alan@hotmail.com>
2022-01-13 20:24:13 +1000

    feat(survey feature): survey feature for survey page

---
+++ src/features/Survey/Survey.tsx
@@ -0,0 +1,73 @@
+import tw from 'twin.macro'
+import DynamicBar from 'components/DynamicBar'
+import { Survey as SurveyProps } from '../../../types/survey'
+import { Headline } from 'components/Typography'
+import Vote from 'components/Vote'
+import { PrimaryButton } from 'components/Button'
+import { useEffect } from 'react'
+import useMethod from 'hooks/use-method'
+import { useMetadata } from 'contexts/metadata'
+import { Answer } from '../../../types/answer.d'
+import useAsync from 'hooks/use-async'
+import { createAnswer } from 'services/survey'
+
+const Survey = ({ survey }: { survey: SurveyProps }) => {
+  const { run } = useAsync()
+  const { questions, availableCredits, vote, canVote } = useMethod(survey)
+  const { metadata, pageLoad } = useMetadata()
+  const {
+    setup: { credits },
+    language: { thumbsDown, thumbsUp, token },
+  } = survey
+
+  useEffect(() => {
+    pageLoad()
+  }, [])
+
+  const handleSubmit = () => {
+    const answer: Answer = {
+      surveyId: survey.id,
+      questions,
+      researcherId: survey.uid,
+      status: survey.status,
+      time: {
+        ...metadata,
+        submitedAt: new Date().toISOString(),
+      },
+      leftCredits: availableCredits,
+    }
+
+    run(createAnswer(answer))
+  }
+
+  return (
+    <div css={tw`container mx-auto`}>
+      <DynamicBar total={credits} availableCredits={availableCredits} language={token} />
+
+      <div css={tw`flex flex-col items-center space-y-24 mt-20`}>
+        {questions.map((question, index) => (
+          <div key={question.id}>
+            <Headline css={tw`mb-4`}>
+              {index + 1}. {question.statement}
+            </Headline>
+
+            <Vote
+              thumbsDown={thumbsDown}
+              thumbsUp={thumbsUp}
+              total={credits}
+              handleVote={(direction: number) => vote(index, direction)}
+              vote={question.vote}
+              creditSpent={question.credits}
+              canVoteUp={canVote(index, 1)}
+              canVoteDown={canVote(index, -1)}
+            />
+          </div>
+        ))}
+
+        <PrimaryButton onClick={handleSubmit}>Submit</PrimaryButton>
+      </div>
+    </div>
+  )
+}
+
+export default Survey
---
+++ src/features/Survey/index.ts
@@ -0,0 +1,3 @@
+export { default as Main } from './Survey'
+export { default as NotFound } from './NotFound'
+export { default as Loading } from './Loading'
