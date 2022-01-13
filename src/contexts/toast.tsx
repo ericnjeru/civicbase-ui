0328d5f21771f7b8581f2d41db7deb8481c22cd3
Alan <mortoni.alan@hotmail.com>
2022-01-13 20:26:24 +1000

    feat(toast context): add toast context

---
+++ src/contexts/toast.tsx
@@ -0,0 +1,41 @@
+import { createContext, ReactElement, useCallback, useContext, useState } from 'react'
+
+interface Toast {
+  text?: string
+}
+
+interface ToastContextProps extends Toast {
+  toast: Toast
+  trigger: (text: string) => void
+}
+
+const initialData: ToastContextProps = {
+  toast: { text: undefined },
+  trigger: () => {},
+}
+
+const ToastContext = createContext(initialData)
+
+export const ToastProvider = (props: any): ReactElement => {
+  const [toast, setToast] = useState<Toast>()
+
+  const trigger = useCallback((text: string) => {
+    setToast({ text })
+
+    setTimeout(() => {
+      setToast({ text: undefined })
+    }, 5000)
+  }, [])
+
+  return <ToastContext.Provider value={{ trigger, toast }} {...props} />
+}
+
+export const useToast = () => {
+  const context = useContext(ToastContext)
+
+  if (context === undefined) {
+    throw new Error('useToast() must be use within a ToastProvider')
+  }
+
+  return context
+}
