88ce56d7c0066c60b60579851c2acbfa096f5b35
Alan <mortoni.alan@hotmail.com>
2022-01-13 20:37:11 +1000

    fix(survey form submit button): contional 'Edit' or 'Create' form

--- src/components/forms/Survey/Submit.tsx
+++ src/components/forms/Survey/Submit.tsx
@@ -1,18 +1,22 @@
 import tw from 'twin.macro'
 import { useFormContext } from 'react-hook-form'
 import { PrimaryButton } from 'components/Button'
+import Spinner from 'components/Spinner'
 
-const SubmitSurvey = () => {
+const SubmitSurvey = ({ isLoading, isEditing = false }: { isLoading: boolean; isEditing?: boolean }) => {
   const {
     formState: { isValid, isDirty },
   } = useFormContext()
 
-  console.log('isValid', isValid)
-
   return (
     <div>
-      <PrimaryButton css={tw`w-full`} type="submit" disabled={!isValid || !isDirty}>
-        Create Survey
+      <PrimaryButton
+        css={tw`w-full flex justify-center items-center`}
+        type="submit"
+        disabled={!isValid || !isDirty || isLoading}
+      >
+        {isLoading && <Spinner variant="light" css={tw`mr-4`} />}
+        {isEditing ? 'Edit' : 'Create'} Survey
       </PrimaryButton>
     </div>
   )
