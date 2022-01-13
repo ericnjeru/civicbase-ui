9f782f757d92e13525f4589d7d23b54df5faadaf
Alan <mortoni.alan@hotmail.com>
2022-01-13 20:20:19 +1000

    feat(researcher survey page): add page for preview survey page for researcher

---
+++ src/pages/survey/Researcher.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import { RouteComponentProps } from '@reach/router'
+
+const ResearcherSurvey: FC<RouteComponentProps> = () => {
+  return (
+    <div>
+      <div>Survey details page for researcher</div>
+    </div>
+  )
+}
+
+export default ResearcherSurvey
