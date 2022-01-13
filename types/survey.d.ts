10baca5d46aef68931ceed0575605863809d937a
Alan <mortoni.alan@hotmail.com>
2022-01-13 20:13:45 +1000

    fix(types): update survey types

--- types/survey.d.ts
+++ types/survey.d.ts
@@ -1,34 +1,37 @@
 export interface Survey {
   setup: Setup
-  language: Langage
+  language: Language
   message?: Message
   questions: Question[]
   features?: Features
   qualtrics?: Qualtrics
   analytics: Analytics
-  id?: string
-  createdAt?: string
-  uid?: string
-  status?: 'pilot' | 'published' | 'finished'
+  id: string
+  createdAt: string
+  uid: string
+  status: Status
+  publishedAt?: string
+  finishedAt?: string
 }
+
+export type Status = 'pilot' | 'published' | 'finished'
 
 export type Setup = {
   topic: string
-  function: 'linear' | 'quadratic'
+  method: 'Linear' | 'Quadratic'
   credits: number
 }
 
-export type Langage = {
+export type Language = {
   token: string
-  jargon: {
-    thumbsUp: string
-    thumbsDown: string
-  }
+  thumbsUp: string
+  thumbsDown: string
+  jargon: string
 }
 
 export type Message = {
-  welcome?: string
-  completion?: string
+  welcome?: any
+  completion?: any
 }
 
 export type Question = {
@@ -54,4 +57,9 @@ export type Analytics = {
     respondents: number
     access: number
   }
+}
+
+export interface QuestionForSurvey extends Question {
+  vote: number
+  credits: number
 }
