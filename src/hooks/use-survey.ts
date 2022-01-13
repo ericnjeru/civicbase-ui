ac48a9ac8648191a141a3bc562e0847cc7a389bb
Alan <mortoni.alan@hotmail.com>
2022-01-13 20:23:15 +1000

    feat(use survey): get survey

---
+++ src/hooks/use-survey.ts
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+import { getSurvey } from 'services/survey'
+import { Survey } from '../../types/survey'
+import useAsync from './use-async'
+
+type UseSurvey = {
+  data: Survey
+  isLoading: boolean
+  isError: boolean
+  isSuccess: boolean
+}
+
+const useSurvey = (surveyId?: string): UseSurvey => {
+  const { run, data, isLoading, isError, isSuccess } = useAsync()
+
+  useEffect(() => {
+    if (surveyId) {
+      run(getSurvey(surveyId))
+    }
+  }, [run, surveyId])
+
+  return {
+    data,
+    isSuccess,
+    isLoading,
+    isError,
+  }
+}
+
+export default useSurvey
