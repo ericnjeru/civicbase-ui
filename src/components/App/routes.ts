0c67f920d9ef2ab8bc28e771ae60283f932b3c9c
Alan <mortoni.alan@hotmail.com>
2022-01-13 20:39:42 +1000

    feat(routes): add survey preview for researcher router

--- src/components/App/routes.ts
+++ src/components/App/routes.ts
@@ -1,8 +1,9 @@
 export const AUTHENTICATED_ROUTES = {
   DASHBOARD: '/',
   CREATE_SURVEY: '/create-survey',
+  EDIT_SURVEY: '/edit-survey',
   ANALYTICS: '/analytics/:surveyId',
-  SURVEY_DETAILS: '/survey-details/:surveyId',
+  SURVEY: '/survey/:surveyId',
   FAQ: '/FAQ',
 }
 
