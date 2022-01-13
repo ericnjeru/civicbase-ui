7df7a7e3ea9d81555b10290803fdaff233163270
Alan <mortoni.alan@hotmail.com>
2022-01-13 20:41:16 +1000

    feat(types): add CreateAnswerRequest type

--- functions/types/survey.d.ts
+++ functions/types/survey.d.ts
@@ -5,6 +5,26 @@ export type CreateRequest = {
   [key: string]: any // not happy with this, but the only way I found so far to add user to the request
 }
 
-export type UpdateRequest = {
-  body: Survey
+export type CreateAnswerRequest = {
+  body: {
+    surveyId: string
+    questions: AnswerQuestion[]
+    researcherId: string
+    status: string
+    time: {
+      startAt: string
+      submitedAt: string
+      pageLoadAt: string
+      questionPageLoadAt: string
+    }
+    leftCredits: number
+    userId?: string
+  }
+}
+
+type AnswerQuestion = {
+  id: string
+  statement: string
+  vote: number
+  credits: number
 }
