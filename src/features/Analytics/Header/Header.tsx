c30bd8702b02fccf0a8c7a5c5022a81e2235c7fb
Alan <mortoni.alan@hotmail.com>
2022-02-05 19:24:19 +1000

    fix(analytics): show correct token and jargon

--- src/features/Analytics/Header/Header.tsx
+++ src/features/Analytics/Header/Header.tsx
@@ -3,19 +3,26 @@ import tw from 'twin.macro'
 import { Survey } from '../../../../types/survey-base'
 
 const Header = ({ survey }: { survey: Survey }) => {
+  const {
+    setup: { method, topic, credits },
+    language: { token, customToken, thumbsDown, thumbsUp },
+  } = survey
+
   return (
     <div css={tw`flex flex-col items-center justify-center`}>
       <Headline css={tw`text-brand`}>SURVEY ANALYTICS</Headline>
-      <Title css={tw`text-6xl`}>{survey.setup.topic}</Title>
+      <Title css={tw`text-6xl`}>{topic}</Title>
       <div css={tw`text-center max-w-2xl`}>
         <Typography css={tw`text-gray-500 inline-block`}>This survey is setup with</Typography>{' '}
-        <Typography css={tw`text-brand2 inline-block`}>{survey.setup.method}</Typography>{' '}
+        <Typography css={tw`text-brand2 inline-block`}>{method}</Typography>{' '}
         <Typography css={tw`text-gray-500 inline-block`}>as preferred function. Each respondent will have</Typography>{' '}
         <Typography css={tw`text-brand2 inline-block`}>
-          {survey.setup.credits} {survey.language.token},
+          {credits} {token === 'Custom' ? customToken : token},
         </Typography>{' '}
         <Typography css={tw`text-gray-500 inline-block`}>the language designated is</Typography>{' '}
-        <Typography css={tw`text-brand2 inline-block`}>{survey.language.jargon}.</Typography>
+        <Typography css={tw`text-brand2 inline-block`}>
+          {thumbsUp}/{thumbsDown}.
+        </Typography>
       </div>
     </div>
   )
