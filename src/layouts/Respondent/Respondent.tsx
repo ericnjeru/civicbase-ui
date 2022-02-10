24ba84b717231e27cc79c9bef2acb608486e9d3e
Alan <mortoni.alan@hotmail.com>
2022-02-11 09:55:44 +1000

    feat(respondent layout): layout for all surveys types

---
+++ src/layouts/Respondent/Respondent.tsx
@@ -0,0 +1,33 @@
+import Container from 'components/Container'
+import { ReactNode } from 'react'
+import tw from 'twin.macro'
+
+const RespondentLayout = ({
+  header,
+  main,
+  feedback,
+  footer,
+}: {
+  header?: ReactNode
+  main?: ReactNode
+  feedback?: ReactNode
+  footer: ReactNode
+}) => {
+  return (
+    <div id="respondent">
+      {header && <>{header}</>}
+
+      {main && <Container id="main">{main}</Container>}
+
+      {feedback && <Container id="feedback">{feedback}</Container>}
+
+      {footer && (
+        <div css={tw`text-center pb-20`}>
+          <Container id="footer">{footer}</Container>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default RespondentLayout
