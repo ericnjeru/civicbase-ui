8c0ff5dc9b48595d4e2d3118642b7ecb831dd275
Alan <mortoni.alan@hotmail.com>
2022-02-23 16:06:53 +1000

    fix(banner): storybook

--- src/components/Banner/Banner.stories.tsx
+++ src/components/Banner/Banner.stories.tsx
@@ -1,16 +1,23 @@
-import { Meta } from '@storybook/react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { BannerProvider } from 'contexts/banner'
 import tw from 'twin.macro'
 import Banner from './Banner'
 
 export default {
-  title: 'Elements/Banner',
+  title: 'Components/Banner',
   component: Banner,
-} as Meta
+} as ComponentMeta<typeof Banner>
 
-export const Basic = () => (
-  <div css={tw`w-screen flex justify-center h-80`}>
-    <div css={tw`w-5/6`}>
-      <Banner />
+const Template: ComponentStory<typeof Banner> = (args) => (
+  <div css={tw`w-screen`}>
+    <div css={tw`container`}>
+      <BannerProvider {...args}>
+        <Banner />
+      </BannerProvider>
     </div>
   </div>
 )
+
+export const Basic = Template.bind({})
+
+Basic.args = { value: { show: true, title: 'title', subtitle: 'subtitle', actionText: 'action', action: () => {} } }
