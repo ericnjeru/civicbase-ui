9b5e99d2b0d09c1c00708c9d73acf1b17db1cf66
Alan <mortoni.alan@hotmail.com>
2022-02-23 16:08:47 +1000

    fix(create survey): storybook

--- src/components/forms/Survey/Survey.stories.tsx
+++ src/components/forms/Survey/Survey.stories.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import { Meta } from '@storybook/react'
-import Survey from './Survey'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 import tw from 'twin.macro'
+import { createHistory, LocationProvider } from '@reach/router'
+import Survey from './Survey'
 
 export default {
   title: 'Forms/Survey',
   component: Survey,
-} as Meta
+} as ComponentMeta<typeof Survey>
 
-export const Basic = () => (
-  <div css={tw`w-screen max-w-screen-lg`}>
-    <Survey />
-  </div>
-)
+const Template: ComponentStory<typeof Survey> = () => {
+  const history = createHistory(window as any)
+
+  return (
+    <div css={tw`w-screen max-w-screen-lg h-screen`}>
+      <LocationProvider history={history}>
+        <Survey />
+      </LocationProvider>
+    </div>
+  )
+}
+
+export const Basic = Template.bind({})
+
+Basic.args = {}
