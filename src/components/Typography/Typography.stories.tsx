5a1cb4e865f70ce3d6089e8df6ec230917af7570
Alan <mortoni.alan@hotmail.com>
2022-02-23 16:11:10 +1000

    fix(typography): storybook

--- src/components/Typography/Typography.stories.tsx
+++ src/components/Typography/Typography.stories.tsx
@@ -6,6 +6,7 @@ import Typography from './Typography'
 import Caption from './Caption'
 import Headline from './Headline'
 import Hint from './Hint'
+import tw from 'twin.macro'
 
 export default {
   title: 'Elements/Typography',
@@ -13,24 +14,36 @@ export default {
 } as Meta
 
 export const Basic = () => (
-  <>
-    <div>
-      <Title>Title: Vitae delectus at facilis blanditiis corrupti dolor laboriosam repellendus</Title>
+  <div css={tw`grid grid-cols-6 gap-4`}>
+    <Typography css={tw`flex justify-center items-center`}>Title</Typography>
+
+    <div css={tw`col-span-5`}>
+      <Title>Vitae delectus at facilis blanditiis corrupti dolor laboriosam repellendus</Title>
     </div>
-    <div>
-      <Subtitle>Subtitle: Vitae delectus at facilis blanditiis corrupti dolor laboriosam repellendus</Subtitle>
+
+    <Typography css={tw`flex justify-center items-center`}>Subtitle</Typography>
+    <div css={tw`col-span-5`}>
+      <Subtitle> Vitae delectus at facilis blanditiis corrupti dolor laboriosam repellendus</Subtitle>
     </div>
-    <div>
-      <Typography>Typography: Vitae delectus at facilis blanditiis corrupti dolor laboriosam repellendus</Typography>
+
+    <Typography css={tw`flex justify-center items-center`}>Typography</Typography>
+    <div css={tw`col-span-5`}>
+      <Typography>Vitae delectus at facilis blanditiis corrupti dolor laboriosam repellendus</Typography>
     </div>
-    <div>
-      <Caption>Caption: Vitae delectus at facilis blanditiis corrupti dolor laboriosam repellendus</Caption>
+
+    <Typography css={tw`flex justify-center items-center`}>Caption</Typography>
+    <div css={tw`col-span-5`}>
+      <Caption>Vitae delectus at facilis blanditiis corrupti dolor laboriosam repellendus</Caption>
     </div>
-    <div>
-      <Headline>Headline: Vitae delectus at facilis blanditiis corrupti dolor laboriosam repellendus</Headline>
+
+    <Typography css={tw`flex justify-center items-center`}>Headline</Typography>
+    <div css={tw`col-span-5`}>
+      <Headline>Vitae delectus at facilis blanditiis corrupti dolor laboriosam repellendus</Headline>
     </div>
-    <div>
-      <Hint>Hint: Vitae delectus at facilis blanditiis corrupti dolor laboriosam repellendus</Hint>
+
+    <Typography css={tw`flex justify-center items-center`}>Hint</Typography>
+    <div css={tw`col-span-5`}>
+      <Hint>Vitae delectus at facilis blanditiis corrupti dolor laboriosam repellendus</Hint>
     </div>
-  </>
+  </div>
 )
