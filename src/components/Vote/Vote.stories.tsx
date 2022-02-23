5b57d0e77322664e07432f19634e27d4e87c8e93
Alan <mortoni.alan@hotmail.com>
2022-02-23 16:10:41 +1000

    fix(vote): storybook

--- src/components/Vote/Vote.stories.tsx
+++ src/components/Vote/Vote.stories.tsx
@@ -1,21 +1,25 @@
-import { Meta } from '@storybook/react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 import Vote from './Vote'
 
 export default {
   title: 'Components/Vote',
   component: Vote,
-} as Meta
+} as ComponentMeta<typeof Vote>
 
-export const Basic = () => (
-  <Vote
-    thumbsUp="Agree"
-    thumbsDown="Disagree"
-    handleVote={() => {}}
-    vote={0}
-    total={100}
-    creditSpent={0}
-    canVoteUp={true}
-    canVoteDown={true}
-    token="Credits"
-  />
-)
+const Template: ComponentStory<typeof Vote> = (args) => {
+  return <Vote {...args} />
+}
+
+export const Basic = Template.bind({})
+
+Basic.args = {
+  thumbsUp: 'Agree',
+  thumbsDown: 'Disagree',
+  vote: 0,
+  total: 100,
+  creditSpent: 0,
+  canVoteUp: true,
+  canVoteDown: true,
+  token: 'Credits',
+  handleVote: () => {},
+}
