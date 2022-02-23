3e87a139915b81d75eb6d5c336791a3199ab3847
Alan <mortoni.alan@hotmail.com>
2022-02-23 16:08:28 +1000

    fix(input): storybook

--- src/components/Form/Input/Input.stories.tsx
+++ src/components/Form/Input/Input.stories.tsx
@@ -1,52 +1,55 @@
-import { Meta } from '@storybook/react'
+// import { Meta } from '@storybook/react'
 import { IconButton } from 'components/Button'
 import { AiOutlineClose } from 'react-icons/ai'
-import Input from './Input'
+// import Input from './Input'
 import Label from 'components/Form/Label'
-import tw from 'twin.macro'
+// import tw from 'twin.macro'
 import CustomInput from './CustomInput'
 import { useForm, FormProvider } from 'react-hook-form'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+// import { useState } from 'react'
+import tw from 'twin.macro'
+import Input from './Input'
 
 export default {
   title: 'Elements/Input',
   component: Input,
-} as Meta
+} as ComponentMeta<typeof Input>
 
-export const Basic = () => (
-  <>
-    <Label>Name</Label>
-    <Input />
-  </>
-)
+const Template: ComponentStory<typeof Input> = (args) => {
+  return (
+    <div css={tw`w-72`}>
+      <Label>Name</Label>
+      <Input error={args.error} modified={args.modified} disabled={args.disabled} />
+    </div>
+  )
+}
 
-export const WithError = () => (
-  <>
-    <Label>Name</Label>
-    <Input error={true} />
-  </>
-)
+export const Basic = Template.bind({})
 
-export const Custom = () => {
+Basic.args = {
+  error: false,
+  modified: false,
+  disabled: false,
+}
+
+const CustomTemplate: ComponentStory<typeof CustomInput> = () => {
   const methods = useForm()
 
   return (
-    <FormProvider {...methods}>
-      <Label>Name</Label>
-      <CustomInput index="TST" name="field">
-        <IconButton onClick={() => {}} css={tw`hover:bg-red-50 mr-1`}>
-          <AiOutlineClose />
-        </IconButton>
-      </CustomInput>
-    </FormProvider>
+    <div css={tw`w-72`}>
+      <FormProvider {...methods}>
+        <Label>Name</Label>
+        <CustomInput index="TST" name="field">
+          <IconButton onClick={() => {}} css={tw`hover:bg-red-50 mr-1`}>
+            <AiOutlineClose />
+          </IconButton>
+        </CustomInput>
+      </FormProvider>
+    </div>
   )
 }
-export const CustomWithoutActionButton = () => {
-  const methods = useForm()
 
-  return (
-    <FormProvider {...methods}>
-      <Label>Name</Label>
-      <CustomInput index="TST" name="field" />
-    </FormProvider>
-  )
-}
+export const Custom = CustomTemplate.bind({})
+
+Custom.args = {}
