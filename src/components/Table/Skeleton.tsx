e9d8a925bd0f525e2af32c123afacad9fbf348bf
Alan <mortoni.alan@hotmail.com>
2022-02-25 21:19:48 +1000

    feat(table): skeleton

---
+++ src/components/Table/Skeleton.tsx
@@ -0,0 +1,37 @@
+import tw, { styled } from 'twin.macro'
+import * as Table from './index'
+
+const TextLine = styled.div(() => tw`bg-gray-200 rounded-full animate-pulse`)
+
+const Skeleton = ({ cols, rows }: { cols: number; rows: number }) => {
+  const columns = Array.from({ length: cols }, (v, k) => k + 1)
+  const lines = Array.from({ length: rows }, (v, k) => k + 1)
+
+  return (
+    <Table.Main>
+      <Table.Head>
+        <Table.Row>
+          {columns.map((key) => (
+            <Table.Data key={key}>
+              <TextLine css={tw`h-6 w-24`} />
+            </Table.Data>
+          ))}
+        </Table.Row>
+      </Table.Head>
+
+      <Table.Body>
+        {lines.map((rowKey) => (
+          <Table.Row key={rowKey}>
+            {columns.map((key) => (
+              <Table.Data key={key}>
+                <TextLine css={tw`h-6 w-24`} />
+              </Table.Data>
+            ))}
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Main>
+  )
+}
+
+export default Skeleton
--- src/components/Table/Table.stories.tsx
+++ src/components/Table/Table.stories.tsx
@@ -1,28 +1,48 @@
-import { Meta } from '@storybook/react'
-import * as Table from './index'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 import tw from 'twin.macro'
+import * as Table from './index'
+import SkeletonTable from './Skeleton'
 
 export default {
   title: 'Elements/Table',
   component: Table.Main,
-} as Meta
+} as ComponentMeta<typeof Table.Main>
 
-export const Basic = () => (
+const Template: ComponentStory<typeof Table.Main> = () => {
+  return (
+    <div css={tw`w-screen max-w-screen-lg`}>
+      <Table.Main>
+        <Table.Head>
+          <Table.Row>
+            <Table.Header>Name</Table.Header>
+            <Table.Header>Title</Table.Header>
+          </Table.Row>
+        </Table.Head>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Data>Alan</Table.Data>
+            <Table.Data>MR</Table.Data>
+          </Table.Row>
+        </Table.Body>
+      </Table.Main>
+    </div>
+  )
+}
+
+export const Basic = Template.bind({})
+
+Basic.args = {}
+
+export const SkeletonTemplace: ComponentStory<typeof SkeletonTable> = (args) => (
   <div css={tw`w-screen max-w-screen-lg`}>
-    <Table.Main>
-      <Table.Head>
-        <Table.Row>
-          <Table.Header>Name</Table.Header>
-          <Table.Header>Title</Table.Header>
-        </Table.Row>
-      </Table.Head>
-
-      <Table.Body>
-        <Table.Row>
-          <Table.Data>Alan</Table.Data>
-          <Table.Data>MR</Table.Data>
-        </Table.Row>
-      </Table.Body>
-    </Table.Main>
+    <SkeletonTable {...args} />
   </div>
 )
+
+export const Skeleton = SkeletonTemplace.bind({})
+
+Skeleton.args = {
+  cols: 2,
+  rows: 2,
+}
