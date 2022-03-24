87b003aa9690404f482ad9d5ab7c11b3c93036e8
Alan <mortoni.alan@hotmail.com>
2022-03-24 16:54:28 +1000

    fix(feedback question): length validation increase to 250

--- src/components/forms/Survey/validation.ts
+++ src/components/forms/Survey/validation.ts
@@ -23,7 +23,7 @@ export const validationSchema = z
                 question: z
                   .string()
                   .min(3, { message: 'Feedback question must have more than 2 characters' })
-                  .max(50, { message: 'Feedback question must have less than 50 characters' }),
+                  .max(250, { message: 'Feedback question must have less than 250 characters' }),
               }),
             )
             .optional(),
