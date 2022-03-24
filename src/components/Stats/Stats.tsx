f513c78184b3f851d075e3e89f6661bbbb51fd79
Alan <mortoni.alan@hotmail.com>
2022-03-25 09:47:09 +1000

    fix(stats component): set border to transparent

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -4,7 +4,7 @@ import tw from 'twin.macro'
 
 const Stats = ({ children }: { children: ReactNode[] }) => {
   return (
-    <Card css={tw`p-0`}>
+    <Card css={tw`p-0 border-transparent`}>
       <div css={tw`grid grid-flow-col divide-x dark:(divide-gray-700 hover:divide-gray-800)`}>{children}</div>
     </Card>
   )
