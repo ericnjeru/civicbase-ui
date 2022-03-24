7b86ab01f83bc0561fb2ef2a7f05f5cc29afb524
Alan <mortoni.alan@hotmail.com>
2022-03-24 17:30:26 +1000

    feat(stats component): dark theme

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -5,7 +5,7 @@ import tw from 'twin.macro'
 const Stats = ({ children }: { children: ReactNode[] }) => {
   return (
     <Card css={tw`p-0`}>
-      <div css={tw`grid grid-flow-col divide-x`}>{children}</div>
+      <div css={tw`grid grid-flow-col divide-x dark:(divide-gray-700 hover:divide-gray-800)`}>{children}</div>
     </Card>
   )
 }
