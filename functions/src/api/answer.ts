9c7e792f33692dfa98cfff0e8da13d3b69285535
Alan Mortoni <mortoni.alan@hotmail.com>
2022-07-19 00:03:46 +1000

    fix(answers): add answer as subcolletions to surveys

--- functions/src/api/answer.ts
+++ functions/src/api/answer.ts
@@ -1,39 +1,18 @@
-import { CreateAnswerRequest } from '../../types/survey'
-import { db, admin } from '../config/firebase'
 import { Response } from 'express'
+import { CreateAnswerRequest } from '../../types/survey'
+import { db } from '../config/firebase'
 import { incrementRespondent } from '../utils/survey'
-import { v4 as uuidv4 } from 'uuid'
 
 export const createAnswer = (req: CreateAnswerRequest, res: Response) => {
   const answer = {
     ...req.body,
     createdAt: new Date().toISOString(),
-    id: uuidv4(),
   }
 
-  db.doc(`/answers/${answer.surveyId}`)
-    .get()
-    .then((doc) => {
-      if (doc.exists) {
-        const document = db.collection('answers').doc(answer.surveyId)
-
-        document.update({ answers: admin.firestore.FieldValue.arrayUnion(answer) }).then(() => {
-          incrementRespondent(answer.surveyId, answer.status)
-        })
-      } else {
-        db.collection('answers')
-          .doc(answer.surveyId)
-          .set({})
-          .then(() => {
-            const document = db.collection('answers').doc(answer.surveyId)
-
-            document.update({ answers: admin.firestore.FieldValue.arrayUnion(answer) }).then(() => {
-              incrementRespondent(answer.surveyId, answer.status)
-            })
-          })
-      }
-
-      res.status(201).json(doc.id)
-    })
+  db.collection('surveys')
+    .doc(answer.surveyId)
+    .collection('answers')
+    .add(answer)
+    .then((doc) => incrementRespondent(answer.surveyId, answer.status).then(() => res.status(201).json(doc.id)))
     .catch((error) => res.status(500).json(error))
 }
