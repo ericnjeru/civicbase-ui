65eec84556ca94e44cc4bd782b54951b5e0b6514
Alan Mortoni <mortoni.alan@hotmail.com>
2022-10-31 22:09:38 +1000

    fix(functions): delete functions

--- functions/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testTimeout: 120000,
-  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
-  testMatch: ['<rootDir>/tests/**/*.test.ts', '<rootDir>/src/api/**/*.test.ts'],
-}
